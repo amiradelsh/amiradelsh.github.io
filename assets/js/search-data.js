5ed41ac9b417225d8158849b551645528fa87e06
amiradelsh <62857717+amiradelsh@users.noreply.github.com>
2025-02-03 14:27:57 +0000

    Deploying to gh-pages from @ amiradelsh/amiradelsh.github.io@0bc0122e0d2278caac9fad912189608706e51ce8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,9 +407,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-parkside",
-          title: '[PARKSIDE]',
-          description: "",
+          section: "News",},{id: "projects-melal-business-hotel",
+          title: '[MELAL BUSINESS HOTEL]',
+          description: "2019 - 2021",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
